475b2ddf111791422b03c5043ef1fc4507bffc1d
Kenneth Liang <61535921+kenaimachine@users.noreply.github.com>
2020-02-28 01:29:21 +0800

    Update panelserverextension.py

--- panelserverextension.py
+++ panelserverextension.py
@@ -2,4 +2,4 @@ from subprocess import Popen
 
 def load_jupyter_server_extension(nbapp):
     """serve the hdbRent.ipynb directory with bokeh server"""
-    Popen(["panel", "serve", "hdbRent.ipynb", "--allow-websocket-origin=*"])
+    Popen(["panel", "serve", "hdbRent", "--allow-websocket-origin=*"])
